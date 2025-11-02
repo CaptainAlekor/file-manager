3bdd72f7079e2ba11d9b8f5acb3c1512348d0587
CaptainAlekor <sashakorostelev49@gmail.com>
2025-11-02 20:02:46 +0300

    feat: add 'cd' command

--- src/fileManager.js
+++ src/fileManager.js
@@ -1,31 +1,50 @@
 import * as path from 'node:path';
 import * as os from 'node:os';
+import { stat } from 'fs/promises';
+
+const MSG_OPERATION_FAILED = 'Operation failed';
+const MSG_INVALID_INPUT = 'Invalid input';
 
 const commands = {
     'up': (_, currentDirectory) => {
         return path.join(currentDirectory, '..');
-    }
-}
+    },
+    'cd': async (args, currentDirectory) => {
+        if (!args[0]) {
+            console.log(MSG_INVALID_INPUT);
+            return currentDirectory;
+        }
+
+        const newDirectory = path.join(currentDirectory, args[0]);
+
+        if ((await stat(newDirectory)).isDirectory() === false) {
+            console.log(MSG_OPERATION_FAILED);
+            return currentDirectory;
+        }
+
+        return newDirectory;
+    },
+};
 
 function runFileManager() {
     let currentDirectory = os.homedir();
 
     printCurrentDirectory(currentDirectory);
 
-    process.stdin.on('data', (data) => {
-        currentDirectory = handleUserInput(data.toString().trim(), currentDirectory);
+    process.stdin.on('data', async (data) => {
+        currentDirectory = await handleUserInput(data.toString().trim(), currentDirectory);
         printCurrentDirectory(currentDirectory);
     });
 }
 
-function handleUserInput(input, currentDirectory) {
+async function handleUserInput(input, currentDirectory) {
     const commandParts = input.split(' ');
     const command = commandParts[0];
     const processCommand = commands[command];
     if (processCommand) {
-        currentDirectory = processCommand(commandParts.slice(1), currentDirectory);
+        currentDirectory = await processCommand(commandParts.slice(1), currentDirectory);
     } else {
-        console.log(`Unknown command: ${command}`);
+        console.log(MSG_INVALID_INPUT);
     }
     return currentDirectory;
 }
