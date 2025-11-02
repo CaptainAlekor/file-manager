945e73b4224ce1b1c6101554ef8edc5f7b648c51
CaptainAlekor <sashakorostelev49@gmail.com>
2025-11-02 20:48:43 +0300

    feat: add 'ls' command

--- src/fileManager.js
+++ src/fileManager.js
@@ -1,6 +1,6 @@
 import * as path from 'node:path';
 import * as os from 'node:os';
-import { stat } from 'fs/promises';
+import { stat, readdir } from 'fs/promises';
 
 const MSG_OPERATION_FAILED = 'Operation failed';
 const MSG_INVALID_INPUT = 'Invalid input';
@@ -30,6 +30,34 @@ const commands = {
         }
 
         return nextDirectory;
+    },
+    'ls': async (_, currentDirectory) => {
+        const directoryEntries = (await readdir(currentDirectory, { withFileTypes: true }))
+            .map(entry => {
+                return {
+                    name: entry.name,
+                    type: (() => {
+                        if (entry.isFile()) {
+                            return 'file';
+                        } else if (entry.isDirectory()) {
+                            return 'directory';
+                        } else {
+                            return 'other';
+                        }
+                    })(),
+                };
+            })
+            .sort((a, b) => a.name.localeCompare(b.name));
+
+        const sortedDirectoryEntries = [
+            ...directoryEntries.filter(entry => entry.type === 'directory'),
+            ...directoryEntries.filter(entry => entry.type === 'file'),
+            ...directoryEntries.filter(entry => entry.type === 'other'),
+        ]
+
+        console.table(sortedDirectoryEntries);
+
+        return currentDirectory;
     },
 };
 
