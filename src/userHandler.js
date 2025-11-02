52e73fe56c030f6e32c4a7ed8f9d281bcaef4aef
CaptainAlekor <sashakorostelev49@gmail.com>
2025-11-03 00:55:42 +0300

    fix: fix no username bug

--- src/userHandler.js
+++ src/userHandler.js
@@ -4,7 +4,8 @@ function handleUser() {
     const username = process.argv
         .slice(2)
         .find(arg => arg.startsWith(USERNAME_ARG_IDENTIFIER))
-        .substring(USERNAME_ARG_IDENTIFIER.length);
+        ?.substring(USERNAME_ARG_IDENTIFIER.length)
+        || 'guest';
 
     console.log(`Welcome to the File Manager, ${username}!`);
 
